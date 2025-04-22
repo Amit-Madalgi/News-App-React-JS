587f2406b50e81bd29aaf7e6c203bd0315012683
Amit-Madalgi <amitmadalgi412@gmail.com>
2025-04-22 21:26:11 +0530

    All errors fixed

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Spinner = () => {
+    return (
+      <div className='text-center'>
+        <div className="spinner-border" role="status">
+            <span className="visually-hidden my-3">Loading...</span>
+        </div>
+      </div>
+    )
+  }
+
+  export default Spinner;
